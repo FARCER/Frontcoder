e9756fa8e126248bc2cd11f3e812ee02c537fe89
Anatoliy Salamatin <counter18@yandex.ru>
2018-11-11 23:16:45 +0300

    ЕTypeWriter

---
+++ README.md
@@ -0,0 +1,5 @@
+Репозиторий с проектами канала FrontCoder
+
+Ссылка на канал https://www.youtube.com/c/frontcoder
+
+Чат в телеграм https://t.me/frontcoder 
---
+++ TypeWriter/finish/main.js
@@ -0,0 +1,49 @@
+const TypeWriter = function (txtElement, words, wait = 3000) {
+    this.txtxElement = txtElement;
+    this.words = words;
+    this.wait = wait;
+    this.wordIndex = 0;
+    this.txt = '';
+    this.isDeleting = false;
+    this.type();
+};
+
+TypeWriter.prototype.type = function () {
+    let typeSpeed = 500,
+        currentIndex = this.wordIndex % this.words.length,
+        currentWord = this.words[currentIndex];
+
+
+    if (this.isDeleting) {
+        typeSpeed = typeSpeed / 2;
+        this.txt = currentWord.substring(0, this.txt.length - 1)
+    } else {
+        typeSpeed = 500;
+        this.txt = currentWord.substring(0, this.txt.length + 1)
+    }
+
+    this.txtxElement.innerHTML = this.txt;
+
+
+    if (this.txt === currentWord && !this.isDeleting) {
+        typeSpeed = 3000;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.wordIndex++;
+        this.isDeleting = false;
+    }
+
+
+    setTimeout(() => this.type(), typeSpeed)
+};
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    let txtElement = document.querySelector('.text'),
+        words = JSON.parse(txtElement.getAttribute('data-words')),
+        wait = txtElement.getAttribute('data-wait');
+
+    new TypeWriter(txtElement, words, wait);
+}
