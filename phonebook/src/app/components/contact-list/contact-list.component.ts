9f47033c3b447801b64d82d7d5a049628c2ad444
Anatoliy Salamatin <counter18@yandex.ru>
2019-08-15 23:28:11 +0300

    Angular Phone Book 3

--- phonebook/src/app/components/contact-list/contact-list.component.ts
+++ phonebook/src/app/components/contact-list/contact-list.component.ts
@@ -1,18 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Contact} from "../../models/contact.model";
+import {CONTACTS} from "./contacts.mockup";
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.scss']
 })
-export class ContactListComponent {
-  public contacts: Contact[] = [
-    new Contact('Иванов Иван', '123456789'),
-    new Contact('Александров Александр', '987654321'),
-  ];
+export class ContactListComponent implements OnInit {
+  public contacts: Contact[] = [];
+
+  ngOnInit(): void {
+    this.contacts = CONTACTS;
+  }
 
   public addContact(contact: Contact): void {
     this.contacts.push(contact);
+  }
+
+  public filter(e: KeyboardEvent) {
+    const value = (e.target as HTMLInputElement).value.toLocaleLowerCase();
+    if (value) {
+      this.contacts = this.contacts.filter(contact => contact.name.toLocaleLowerCase().includes(value));
+    } else {
+      this.contacts = CONTACTS;
+    }
   }
 }
---
+++ phonebook/src/app/components/contact-list/contacts.mockup.ts
@@ -0,0 +1,6 @@
+import {Contact} from "../../models/contact.model";
+
+export const CONTACTS: Contact[] = [
+  new Contact('Иванов Иван', '123456789'),
+  new Contact('Александров Александр', '987654321'),
+];
