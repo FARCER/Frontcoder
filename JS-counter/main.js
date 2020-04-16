9d2d5b8ea03a6b718f6d43ef1c5f2514fbcb18b8
Anatoliy Salamatin <counter18@yandex.ru>
2020-04-16 23:11:16 +0300

    JS counter

---
+++ JS-counter/main.js
@@ -0,0 +1,24 @@
+const counter = function () {
+  const btns = document.querySelectorAll('.counter__btn');
+
+
+  btns.forEach(btn => {
+    btn.addEventListener('click', function () {
+      const direction = this.dataset.direction;
+      const inp = this.parentElement.querySelector('.counter__value');
+      const currentValue = +inp.value;
+      let newValue;
+
+      if (direction === 'plus') {
+        newValue = currentValue + 1;
+      } else {
+        newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+      }
+
+      inp.value = newValue;
+    })
+  })
+
+}
+
+counter();
